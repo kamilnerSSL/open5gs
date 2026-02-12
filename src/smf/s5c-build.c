b59495da465f1d8ac1483edfed6a24904d600bf3
kamilnerSSL <kamilner@sslconsult.com>
2026-02-12 18:12:43 +0000

    Additional debug on SMF

--- src/smf/s5c-build.c
+++ src/smf/s5c-build.c
@@ -136,7 +136,7 @@ ogs_pkbuf_t *smf_s5c_build_create_session_response(
         rsp->aggregate_maximum_bit_rate.len = sizeof(ambr);
 
         ogs_debug("    APN-AMBR: DL[%" PRIu64 "] UL[%" PRIu64 "]",
-                sess->session.ambr.downlink, sess->session.ambr.uplink);
+                ambr.downlink, ambr.uplink);
     }
 
     /* PCO */
