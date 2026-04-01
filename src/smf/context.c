c1ae3729b0b4a33898f321a4e763dba4dea62ad4
kamilnerSSL <kamilner@sslconsult.com>
2026-04-01 11:25:10 +0100

    Adds no-op support for PCO option 0x1a (IP Addr allocation via NAS)

--- src/smf/context.c
+++ src/smf/context.c
@@ -3328,6 +3328,10 @@ int smf_pco_build(uint8_t *pco_buf, uint8_t *buffer, int length)
             smf.ids[smf.num_of_id].data = 0;
             smf.num_of_id++;
             break;
+        case OGS_PCO_ID_IP_ADDRESS_ALLOCATION_VIA_NAS_SIGNALLING:
+            /* The UE indicates that it will only use the IP address allocated via NAS signalling. */
+            /* The network does not include this PCO in the response. */
+            break;
         case OGS_PCO_ID_P_CSCF_RE_SELECTION_SUPPORT:
             /* TODO */
             break;
